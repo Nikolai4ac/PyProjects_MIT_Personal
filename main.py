558768ce301ee714764fdac0f64cbe13b0e44264
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2022-12-17 18:14:51 +0200

    Fixed username check

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Add choice for registration or login at startup
 from tinydb import TinyDB  # for database creation
-from tinydb import Query  # for query lookup
+from tinydb import Query, where  # for query lookup
 import bcrypt  # encrypt the passwords
-# import os
 to_operate = Query()
+toFind = where('key')
+db = TinyDB('C:/Users/xtrop/Desktop/PyProjects/bin/tinydb/db.json')
 
 
 def check_value(val):
@@ -12,24 +13,37 @@ def check_value(val):
         print("Username not found!")
 
 
+def check_user(value):
+    db_user = db.get(where("username") == value)
+    while db_user is not None:
+        value = str(input("Enter test: "))
+        check_available = db.get(to_operate.username == value)
+        if check_available is None:
+            print("Username available!")
+            break
+
+
 def main_menu_choices():
-    print("Register or Login?\n 1-Register\n 2-Login")
+    print("Register or Login? \n0-Exit\n1-Register\n2-Login\n")
 
 
 username_not_found_counter = 0
-db = TinyDB('C:/Users/xtrop/Desktop/PyProjects/bin/tinydb/db.json')
 while True:
     main_menu_choices()
     answer = int(input("Enter answer: "))
+    if answer == 0:
+        break
     if answer == 1:
         username = input("Enter username /0 to exit/: ")
+        check_user(username)
         if username == "0":
             break
-    while len(username) <= 0:
+    while len(username) == 0:
         username = input("Enter valid username: ")
+        check_user(username)
     if len(username) > 0:
         password = str(input("Enter password /0 to exit/: "))
-        while len(password) <= 0:
+        while len(password) == 0:
             password = str(input("Enter valid password /0 to exit/: "))
             if password == "0":
                 break
@@ -42,18 +56,18 @@ while True:
             db.insert(new_profile)  # store username and hashed password as JSON
             print("Successfully registered!")
             print("User: {}".format(username))
-#  TO-DO: Make the login menu to check for the password stored in the Database
-if answer == 2:
-    usernameEnter = input("Enter username: ")
-    check_value(usernameEnter)
-    while db.get(to_operate.username == usernameEnter) is None:
-        username_not_found_counter += 1
-        usernameEnter = input("Enter username: ")
-        check = db.get(to_operate.username == usernameEnter)
-        if check is None:
-            print("Username not found!")
-        if check is True:
-            break
-        if username_not_found_counter == 10:
-            print("Too many attempts, restart the console application")
-            break
+    #  TO-DO: Make the login menu to check for the password stored in the Database
+    if answer == 2:
+        usernameEnter = str(input("Enter username: "))
+        check_value(usernameEnter)
+        while db.get(to_operate.username == usernameEnter) is None:
+            username_not_found_counter += 1
+            usernameEnter = input("Enter username: ")
+            check = db.get(to_operate.username == usernameEnter)
+            if check is None:
+                print("Username not found!")
+            if check is True:
+                break
+            if username_not_found_counter == 10:
+                print("Too many attempts, restart the console application")
+                break
